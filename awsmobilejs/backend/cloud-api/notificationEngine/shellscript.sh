2e574c48bdf6360d90e1483b4845dfe0335049ab
Frank Chau <frank.chau@ralisservices.com>
2018-08-15 21:04:40 -0700

    added ability to save user to DB when signed in + added app data for exporting

--- awsmobilejs/backend/cloud-api/notificationEngine/shellscript.sh
+++ awsmobilejs/backend/cloud-api/notificationEngine/shellscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm /tmp/server0.sock
 MOBILE_HUB_DYNAMIC_PREFIX=happyplaceapp-mobilehub-1689228557 \
 MOBILE_HUB_PROJECT_ID=b7429004-6cdf-4f46-a3d2-a59bdb134c1e MOBILE_HUB_PROJECT_NAME=HappyPlaceApp \
 MOBILE_HUB_PROJECT_REGION=us-west-2 AWS_REGION=us-west-2 REGION=us-west-2 \
